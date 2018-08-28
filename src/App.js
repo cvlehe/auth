d8af6722d9dfc3716de02e64452e6f37ba0380b9
Charles von Lehe <vonlehec@gmail.com>
2018-08-28 17:15:20 -0400

    Added email and password fields

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyARlDlXU-3Eyg3t6woorT0c3GDgEikoUg8',
+      authDomain: 'auth-cd2f8.firebaseapp.com',
+      databaseURL: 'https://auth-cd2f8.firebaseio.com',
+      projectId: 'auth-cd2f8',
+      storageBucket: 'auth-cd2f8.appspot.com',
+      messagingSenderId: '208090503529'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
